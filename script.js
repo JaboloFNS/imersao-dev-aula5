fde23153e135916eb10bcd396df6456012b0a5d4
Fabiano Novais <fabiano.novais.99@gmail.com>
2022-09-11 17:00:36 -0300

    Conteúdo da aula 5 da Imersão Dev, necessário revisão

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ function adicionarFilme() {
         alert("Ensira uma imagem .png ou .jpg");
     }
     document.getElementById("filme").value = " ";
-
 }
+
+// transmitindo uma variável de uma função para a outra através dos parentêses
 function escreverTela(infInput) {
     console.log (infInput);
     var elementoInfInput = "<img src=" + infInput + ">";
     var listaFilmes = document.getElementById("listaFilmes");
     listaFilmes.innerHTML += elementoInfInput;
 }
+
+//teste
